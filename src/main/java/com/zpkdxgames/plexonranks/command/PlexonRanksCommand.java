49c723aebe8b30ff6ea2d7ba1cda0a8b4f705f51
Tonim <99552287+ZpkDxGames@users.noreply.github.com>
2026-09-07 01:31:13 -0300

    fix: restore PlexonRanks command tab completer import

--- src/main/java/com/zpkdxgames/plexonranks/command/PlexonRanksCommand.java
+++ src/main/java/com/zpkdxgames/plexonranks/command/PlexonRanksCommand.java
@@ -23,6 +23,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
